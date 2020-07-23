f067b095f307c2b49bb11c74a25335c2fcaef271
TarasTsyhanko <tarastsyhanko2gmail.com>
2020-07-23 20:09:03 +0300

    create parallel test SearchGoodByWord

---
+++ src/test/java/tests/SearchByWordTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import bo.SearchActions;
+import com.google.inject.Inject;
+import config.PropertyFile;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import validators.SearchValidation;
+
+import java.util.List;
+
+public class SearchByWordTest extends BaseTest {
+    @Inject
+    private SearchActions searchActions;
+    @Inject
+    private SearchValidation validation;
+
+    @DataProvider(name = "SearchDataProvider",parallel = true)
+    private Object[] getData(){
+        Object [] date = {"motorola","iphone","nike"};
+        return date;
+    }
+
+    @Test(description = "search goods by word",dataProvider ="SearchDataProvider" ,
+            threadPoolSize = 3,invocationCount = 1)
+    public void searchGoodsByWord(String word){
+        searchActions.openWebSite(PropertyFile.getProperty("website"));
+        searchActions.findGoodsByText(word);
+        List<String> listTitles = searchActions.getAllTitles();
+        validation.verifyAllTitlesContainsWords(listTitles,word);
+
+    }
+}
