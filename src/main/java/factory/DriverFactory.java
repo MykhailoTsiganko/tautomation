c5b8b937676e86fbdac7ec0c76c3ba204a69168b
TarasTsyhanko <tarastsyhanko2gmail.com>
2020-07-25 22:31:54 +0300

    create method getElement ,write one url for RemoverDriver

--- src/main/java/factory/DriverFactory.java
+++ src/main/java/factory/DriverFactory.java
@@ -1,6 +1,5 @@
 package factory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,9 +18,7 @@ import java.util.function.Supplier;
 
 
 public class DriverFactory {
-    private static final String chromeNode = "http://192.168.56.1:5557/wd/hub";
-    private static final String firefoxNode = "http://192.168.56.1:5556/wd/hub";
-    private static final String edgeNode = "http://192.168.56.1:5558/wd/hub";
+    private static final String clientConnections = "http://192.168.56.1:4444/wd/hub";
     private static final ThreadLocal<WebDriver> parallelDriver = new ThreadLocal<>();
     private static final int implicitlyWait = 25;
 
@@ -50,28 +47,21 @@ public class DriverFactory {
 
     private static WebDriver createWebDriver(String device) throws MalformedURLException {
         MutableCapabilities options;
-        String baseUrl;
         if ("firefox".equals(device)) {
-            WebDriverManager.firefoxdriver().setup();
             options = new FirefoxOptions();
-            baseUrl = firefoxNode;
             options.setCapability("browser", "firefox");
             options.setCapability(FirefoxDriver.MARIONETTE, true);
         } else if ("edge".equals(device)) {
-            WebDriverManager.edgedriver().setup();
             options = new EdgeOptions();
-            baseUrl = edgeNode;
         } else {
-            WebDriverManager.chromedriver().setup();
             options = new ChromeOptions();
-            baseUrl = chromeNode;
             if ("mobile".equals(device)) {
                 setChromeEmulation(options);
             }
         }
         options.setCapability("platform", "WINDOWS");
         options.setCapability("newCommandTimeout", 5000);
-        return new RemoteWebDriver(new URL(baseUrl), options);
+        return new RemoteWebDriver(new URL(clientConnections), options);
     }
 
 
@@ -79,7 +69,8 @@ public class DriverFactory {
         Map<String, String> mobileEmulation = new HashMap<>();
         mobileEmulation.put("deviceName", "iPad");
         String emulator = "mobileEmulation";
-        options.setCapability(emulator, mobileEmulation);
+        ((ChromeOptions) options).setExperimentalOption(emulator,mobileEmulation);
+
     }
 
     public static WebDriver getDiver() {
